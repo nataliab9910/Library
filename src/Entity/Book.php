f250fd764090be7b97525e2cd70513121ea7c32e
Natalia Bidzińska <natalia.b9910@gmail.com>
2021-04-27 18:53:44 +0200

    API

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\BookRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=BookRepository::class)
  */
 class Book
